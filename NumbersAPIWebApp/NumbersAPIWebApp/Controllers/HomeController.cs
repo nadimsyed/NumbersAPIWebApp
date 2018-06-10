54e22d2bbf96749634278d3c17af9c9453e39001
Nadim <syed.m.nadim@gmail.com>
2018-06-10 12:49:56 -0400

    Added Views and logic for the random fact

--- NumbersAPIWebApp/NumbersAPIWebApp/Controllers/HomeController.cs
+++ NumbersAPIWebApp/NumbersAPIWebApp/Controllers/HomeController.cs
@@ -69,9 +69,56 @@ namespace NumbersAPIWebApp.Controllers
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult RandomStart()
         {
-            ViewBag.Message = "Your contact page.";
+            return View();
+        }
+
+        public ActionResult RandomFact(string Choice)
+        {
+            HttpWebRequest request =
+                       WebRequest.CreateHttp($"https://numbersapi.p.mashape.com/random/{Choice}?json=true&max=55&min=25");
+
+
+            request.UserAgent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0";
+
+            // Adding keys to the header 
+            request.Headers.Add("X-Mashape-Key", "OM9KT6qZq0mshX55glqLqujP5PtBp1MtcfzjsnTl6UmlVkd8Tr");
+
+            HttpWebResponse Response;
+
+            try
+            {
+                Response = (HttpWebResponse)request.GetResponse();
+            }
+            catch (WebException ex)
+            {
+                ViewBag.Error = "Exception";
+                ViewBag.ErrorDescription = ex.Message;
+                return View();
+            }
+
+            if (Response.StatusCode != HttpStatusCode.OK)
+            {
+                ViewBag.Error = Response.StatusCode;
+                ViewBag.ErrorDescription = Response.StatusDescription;
+                return View();
+            }
+
+            StreamReader reader = new StreamReader(Response.GetResponseStream());
+            string DateFact = reader.ReadToEnd();
+
+            try
+            {
+                JObject JsonData = JObject.Parse(DateFact);
+                ViewBag.Fact = /*(JObject)*/JsonData["text"];
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = "JSON Issue";
+                ViewBag.ErrorDescription = ex.Message;
+                return View();
+            }
 
             return View();
         }
